12d0803d03a066017559d39a7f2235bfd94295b4
WinSoft <kokarev2015@internet.ru>
2026-02-26 19:20:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('myModal');
+const btnOpen = document.getElementById('openModal');
+const closeBtn = document.getElementById('closeModal');
+
+btnOpen.addEventListener('click', () => {
+  modal.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.classList.remove('show');
+  }
+});
